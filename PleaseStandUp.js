15178903d9783425169cace89578daebbd32a46d
Marcus Smith <mqsmith@Marcuss-MacBook-Air.local>
2021-03-11 19:06:20 -0500

    Please stand up lab rough draft

--- PleaseStandUp.js
+++ PleaseStandUp.js
@@ -7,6 +7,14 @@ class PleaseStandUp {
     */
 
    howOld(birthday){
+       
+    //calculate the month difference from the current month
+       //calculate the day difference from the current date
+       //calculate year difference from the current year
+       //put calculation in date format
+       //calculate the age person
+       //return the age of the person
+
         return -1;
     }
 
@@ -18,9 +26,19 @@ class PleaseStandUp {
     */
 
     timeConvert(numOfMins){
-        return -1;
+        //create a variable to for hours and make it round down and divide it by 3600 seconds
+        var hours = Math.floor(numOfMins/3600);
+        //create a variable for minutes. Once you get hours, then divide it by 60 minutes to get minutes
+        var minutes = Math.floor((numOfMins % 3600)/60);
+        //create a variable. Initial # minus the hours times the total seconds minus the minutes times by 60 to get seconds
+        var seconds = numOfMins - hours * 3600 - minutes * 60;
+        //After calculation, make sure it returns the proper time format
+        return hours + ":" + minutes + ":" + seconds;
     }
-
+    //Be able to plug any number in the console so it can convert(example numbers below)
+    console.log(timeConvert(77));
+    console.log(timeConvert(344));
+    console.log(timeConvert(1299));
 }
 
  module.exports = PleaseStandUp;
